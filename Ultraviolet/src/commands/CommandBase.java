b7637f31915ce79cefe7507d8c1036f0fd261a6a
David Khatami <khatamidk@gmail.com>
2012-07-01 13:09:34 -0500

    Test Commit @David 07/01/12

--- Ultraviolet/src/commands/CommandBase.java
+++ Ultraviolet/src/commands/CommandBase.java
@@ -9,6 +9,7 @@ import subsystems.*;
  * CommandBase stores creates and stores each control system. To access a
  * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
  * @author Author 
+ * #git test
  */
 public abstract class CommandBase extends Command {
 
