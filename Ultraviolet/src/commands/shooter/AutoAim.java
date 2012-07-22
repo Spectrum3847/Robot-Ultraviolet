b2adc1cee471c20bae924cf2dd9bacd43aa6941d
Zachary Meves <zmeves@spectrum3847.org>
2012-07-22 18:51:39 -0500

    redid math class

--- Ultraviolet/src/commands/shooter/AutoAim.java
+++ Ultraviolet/src/commands/shooter/AutoAim.java
@@ -10,123 +10,93 @@ package commands.shooter;
  */
 public class AutoAim {
     
-    //order of pixel data input: topLeft, topRight, bottomRight, bottomLeft
-    public static final double xTotalPixels = 320;
-    public static final double yTotalPixels = 240;
+    //known values
+    public static final double xPixels = 320.0;
+    public static final double yPixels = 240.0;
+    public static final double xFieldRadians = Math.toRadians(47.0);
+    public static final double yFieldRadians = Math.toRadians(35.25);
+    public static final double targetY = 1.5;
+    public static final double cameraTilt = Math.toRadians(20.0);
+    
+    public static double x0,y0,x1,y1,x2,y2,x3,y3;
+    
+    //need to find
+    public static double elevationAngle1, elevationAngle2;   // = yField * y3/2 / yPixels
+    public static double sectionAngle1, sectionAngle2; // = yField * (y0/1 - y3/2) / yPixels
+    public static double alpha1, alpha2; // = 90 -(cameraTilt + elevationAngle + sectionAngle)
+    public static double q1,q2; // = 1.5 * sin(alpha) / sin(sectionAngle)
+    public static double beta1,beta2; // = 180 - sectionAngle - alpha
+    public static double r1,r2; // = 1.5 * sin(beta)/ sin(sectionAngle)
+    public static double s1,s2; // r * sin(alpha) / sin(90)
     
     
-    public static double x0,y0;
-      
-    public static double x1,y1;
-    
-    public static double x2,y2;
-    
-    public static double x3,y3;
-    
-    public static final double cameraTiltAngle = 20.0;
-    public static final double cameraTiltAngleRadians = Math.toRadians(20.0);
-    
-    public static final double basketDimensionY = .4572;
-    
-    public static double getDistance(int[] inputArray){ //inputArray must be 8 long : order [x0,y0,x1,y1,x2,y2,x3,y3]
-         if(inputArray.length != 8) //if array missing elements or too long
+    //0 pairs with 3, 1 pairs with 2;
+    public static double getDistance(int[] inputArray){ //input top left then clockwise
+        if(inputArray.length != 8) //if array missing elements or too long
             return 0;
          
         x0 = (double)inputArray[0]; //initializes doubles
-        y0 = yTotalPixels - (double)inputArray[1];
+        y0 = yPixels - (double)inputArray[1];
         x1 = (double)inputArray[2];
-        y1 = yTotalPixels - (double)inputArray[3];
+        y1 = yPixels - (double)inputArray[3];
         x2 = (double)inputArray[4];
-        y2 = yTotalPixels- (double)inputArray[5];
+        y2 = yPixels- (double)inputArray[5];
         x3 = (double)inputArray[6];
-        y3 = yTotalPixels - (double)inputArray[7];
+        y3 = yPixels - (double)inputArray[7];
     
         if(x0 == 0 || y0 == 0 || x1 == 0 || y1 == 0 || x2 == 0 || y2 == 0 || x3 == 0 || y3 == 0)
             return 0;
         
-        double yPixelDistanceLeft = y0 - y3;
-        double yPixelDistanceRight = y1 - y2;
+        elevationAngle1 = yFieldRadians * y3 / yPixels;
+        elevationAngle2 = yFieldRadians * y2 / yPixels;
         
-        double yAngleLeftRadians = .5694 * yPixelDistanceLeft / 444.255;
-        double yAngleRightRadians = .5694 * yPixelDistanceRight / 444.255;
-
-        double elevationAngleLeftRadians = .5694 * (y3 - 35.745) / 444.255;
-        double elevationAngleRightRadians = .5694 * (y2 - 35.745) / 444.255;
+        sectionAngle1 = yFieldRadians* (y0 - y3) / yPixels;
+        sectionAngle2 = yFieldRadians* (y1 - y2) / yPixels;
         
-        //solving for unknown distances
-        double alphaLeft = Math.sin(Math.PI/2 - (yAngleLeftRadians + elevationAngleLeftRadians)) * .4572 / Math.sin(yAngleLeftRadians);
-        double alphaRight = Math.sin(Math.PI/2 - (yAngleRightRadians + elevationAngleRightRadians)) * .4572 / Math.sin(yAngleRightRadians);
+        alpha1 = Math.toRadians(90.0)-(cameraTilt+elevationAngle1+sectionAngle1);
+        alpha2 = Math.toRadians(90.0)-(cameraTilt+elevationAngle2+sectionAngle2);
         
-        //solving for distances to goal left and right , horizontal distance
-        double distanceLeft = alphaLeft * Math.sin(Math.PI/2 - elevationAngleLeftRadians) / Math.sin(Math.PI/2);
-        double distanceRight = alphaRight * Math.sin(Math.PI/2 - elevationAngleRightRadians) / Math.sin(Math.PI/2);
+        q1 = targetY * Math.sin(alpha1) / Math.sin(sectionAngle1);
+        q2 = targetY* Math.sin(alpha2) / Math.sin(sectionAngle2);
         
-        return (distanceLeft + distanceRight) / 2.0 /.3048; //converts back to feet
+        beta1 = Math.toRadians(180.0) - sectionAngle1 - alpha1;
+        beta2 = Math.toRadians(180.0) - sectionAngle2 - alpha2;
+        
+        r1 = targetY * Math.sin(beta1) / Math.sin(sectionAngle1);
+        r2 = targetY * Math.sin(beta2) / Math.sin(sectionAngle2);
+        
+        s1 = r1*Math.sin(alpha1);
+        s2 = r2*Math.sin(alpha2);
+        
+        return (s1+s2)/2;
     }
     
-    //returns the angle to the center of the goal: positive for angle-correction needed to right, negative for correction needed to left
-    public static double getAngleToGoal(int[] inputArray){
+    public static double getOffsetAngle(int[] inputArray){
         if(inputArray.length != 8) //if array missing elements or too long
             return 0;
         
         x0 = (double)inputArray[0]; //initializes doubles
-        y0 = yTotalPixels - (double)inputArray[1];
+        y0 = yPixels - (double)inputArray[1];
         x1 = (double)inputArray[2];
-        y1 = yTotalPixels - (double)inputArray[3];
+        y1 = yPixels - (double)inputArray[3];
         x2 = (double)inputArray[4];
-        y2 = yTotalPixels - (double)inputArray[5];
+        y2 = yPixels - (double)inputArray[5];
         x3 = (double)inputArray[6];
-        y3 = yTotalPixels - (double)inputArray[7];
+        y3 = yPixels - (double)inputArray[7];
         
         if(x0 == 0 || y0 == 0 || x1 == 0 || y1 == 0 || x2 == 0 || y2 == 0 || x3 == 0 || y3 == 0)
             return 0;
         
-        //from getDistance method
-        double yPixelDistanceLeft = y0 - y3;
-        double yPixelDistanceRight = y1 - y2;
-        
-        double yAngleLeftRadians = .5694 * yPixelDistanceLeft / 444.255;
-        double yAngleRightRadians = .5694 * yPixelDistanceRight / 444.255;
-        
-        double elevationAngleLeftRadians = .5694 * (y3 - 35.745) / 444.255;
-        double elevationAngleRightRadians = .5694 * (y2 - 35.745) / 444.255;
-        
-        //solving for unknown distances
-        double alphaLeft = Math.sin(Math.PI/2 - (yAngleLeftRadians + elevationAngleLeftRadians)) * .4572 / Math.sin(yAngleLeftRadians);
-        double alphaRight = Math.sin(Math.PI/2 - (yAngleRightRadians + elevationAngleRightRadians)) * .4572 / Math.sin(yAngleRightRadians);
-        
-        //solving for distances to goal left and right , horizontal distance
-        double distanceLeft = alphaLeft * Math.sin(Math.PI/2 - elevationAngleLeftRadians) / Math.sin(Math.PI/2);
-        double distanceRight = alphaRight * Math.sin(Math.PI/2 - elevationAngleRightRadians) / Math.sin(Math.PI/2);
-        //end getDistanceMethod
-        
-        double distanceToCorrectPixels;
-        double angleToCorrect;
-        
-        if((x0+x1+x2+x3)/4 > xTotalPixels/2){
-            //need to go to right
-            distanceToCorrectPixels = (x1+x2)/2 - xTotalPixels/2;
-            angleToCorrect = 47.0 * distanceToCorrectPixels / xTotalPixels;
-            return angleToCorrect;
+        double xCenter = xPixels / 2;
+        double total = x0 + x1 + x2 + x3;
+        if(total/4 > xCenter){//too far right
+            return -1 * 47.0 * (xCenter-total/4)/xPixels;
         }
-        else if((x0 + x1 + x2 + x3)/4 < xTotalPixels/2){
-            //need to go to left
-            distanceToCorrectPixels = xTotalPixels/2 - (x0+x3)/2;
-            angleToCorrect = 47.0 * distanceToCorrectPixels / xTotalPixels;
-            return angleToCorrect*-1;
+        else if(total/4 < xCenter){//too far left
+            return -1 * 47.0 * (xCenter - total/4) / xPixels;
         }
-        else return 0;
+        else
+            return 0;
     }
-    
-   public double getRPMLow(int[] inputArray){
-       double distance = getDistance(inputArray);
-       
-       return 0;
-   }
-   
-   public double getRPMHigh(int[] inputArray){
-       double distance = getDistance(inputArray);
-       
-       return 0;
-   }
+
 }
